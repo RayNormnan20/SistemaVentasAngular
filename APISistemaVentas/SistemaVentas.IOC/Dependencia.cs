70bea23fc33feba7c62348223ad5da81e3be617e
RayNormnan20 <josepintadoyamo@gmail.com>
2023-04-22 21:42:03 -0500

    Creacion API

---
+++ APISistemaVentas/SistemaVentas.IOC/Dependencia.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using SistemaVentas.DAL.DBContext;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaVentas.IOC
+{
+    public static class Dependencia
+    {
+        public static void inyectarDependencias(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<DbventaContext>(Options=>
+            {
+                Options.UseSqlServer(configuration.GetConnectionString("cadenaSQl"));
+            });
+        }
+
+    }
+}
