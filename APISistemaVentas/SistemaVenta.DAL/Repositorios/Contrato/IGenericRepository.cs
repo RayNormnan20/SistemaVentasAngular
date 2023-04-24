324725b5015c72dab8d1ee6f586cd6d95097e6de
RayNormnan20 <josepintadoyamo@gmail.com>
2023-04-24 10:42:36 -0500

    Avance 1

---
+++ APISistemaVentas/SistemaVenta.DAL/Repositorios/Contrato/IGenericRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Linq.Expressions;
+
+namespace SistemaVenta.DAL.Repositorios.Contrato
+{
+    public  interface IGenericRepository<TModel>where TModel : class
+    {
+        // Metodo para obtener un modelo
+        Task<TModel> Obtener(Expression<Func<TModel, bool>> filtro);
+        Task<TModel> Crear(TModel modelo);
+        Task<bool> Editar(TModel modelo);
+        Task<bool> Eiminar(TModel modelo);
+        Task<IQueryable<TModel>> Consultar(Expression<Func<TModel, bool>> filtro = null);
+
+    }
+}
---
+++ APISistemaVentas/SistemaVenta.DAL/Repositorios/VentaRepository.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using SistemaVentas.DAL.DBContext;
+using SistemaVenta.DAL.Repositorios.Contrato;
+using SistemaVentas.Model;
+
+namespace SistemaVenta.DAL.Repositorios
+{
+    public class VentaRepository: GenericRepository<Venta>, IVentaRepository
+    {
+        private readonly DbventaContext _dbContext;
+
+        public VentaRepository(DbventaContext dbContext): base(dbContext) 
+        {
+            _dbContext = dbContext;
+        }
+
+
+
+        public Task<Venta> Registar(Venta modelo)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
